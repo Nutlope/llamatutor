9d099faf7a2530fcef2e320638053f410f62b556
Sam Selikoff <sam.selikoff@gmail.com>
2024-07-23 11:39:36 -0400

    Dont scroll if user scrolled to bottom

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,6 +1,6 @@
 import ReactMarkdown from "react-markdown";
 import FinalInputArea from "./FinalInputArea";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import simpleLogo from "../public/simple-logo.png";
 import Image from "next/image";
 
@@ -24,6 +24,8 @@ export default function Chat({
   topic: string;
 }) {
   const messagesEndRef = useRef(null);
+  const scrollableContainerRef = useRef<HTMLDivElement>(null);
+  const [didScrollToBottom, setDidScrollToBottom] = useState(false);
 
   const scrollToBottom = () => {
     // @ts-ignore
@@ -31,8 +33,31 @@ export default function Chat({
   };
 
   useEffect(() => {
-    scrollToBottom();
-  }, [messages]);
+    if (didScrollToBottom) {
+      scrollToBottom();
+    }
+  }, [didScrollToBottom, messages]);
+
+  useEffect(() => {
+    let el = scrollableContainerRef.current;
+    if (!el) {
+      return;
+    }
+
+    function handleScroll() {
+      if (scrollableContainerRef.current) {
+        const { scrollTop, scrollHeight, clientHeight } =
+          scrollableContainerRef.current;
+        setDidScrollToBottom(scrollTop + clientHeight >= scrollHeight);
+      }
+    }
+
+    el.addEventListener("scroll", handleScroll);
+
+    return () => {
+      el.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <div className="flex grow flex-col gap-4 overflow-hidden">
@@ -41,7 +66,10 @@ export default function Chat({
           <b>Topic: </b>
           {topic}
         </p>
-        <div className="mt-2 overflow-y-scroll rounded-lg border border-solid border-[#C2C2C2] bg-white px-5 lg:p-7">
+        <div
+          ref={scrollableContainerRef}
+          className="mt-2 overflow-y-scroll rounded-lg border border-solid border-[#C2C2C2] bg-white px-5 lg:p-7"
+        >
           {messages.length > 2 ? (
             <div className="prose-sm max-w-5xl lg:prose lg:max-w-full">
               {messages.slice(2).map((message, index) =>
