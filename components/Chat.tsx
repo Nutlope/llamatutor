2a90645dd3797317841b7d7e53c54b74c138762c
Kevin Kidd <harrison.kevin@protonmail.com>
2024-07-23 20:38:20 -0400

    style: adjust indentation in multiple files for consistent formatting

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -6,129 +6,129 @@ import Image from "next/image";
 import RelatedTopics from "./RelatedTopics";
 
 export default function Chat({
-	messages,
-	disabled,
-	promptValue,
-	setPromptValue,
-	setMessages,
-	handleChat,
-	topic,
-	handleInitialChat,
+  messages,
+  disabled,
+  promptValue,
+  setPromptValue,
+  setMessages,
+  handleChat,
+  topic,
+  handleInitialChat,
 }: {
-	messages: { role: string; content: string }[];
-	disabled: boolean;
-	promptValue: string;
-	setPromptValue: React.Dispatch<React.SetStateAction<string>>;
-	setMessages: React.Dispatch<
-		React.SetStateAction<{ role: string; content: string }[]>
-	>;
-	handleChat: () => void;
-	topic: string;
-	handleInitialChat: () => Promise<void>;
+  messages: { role: string; content: string }[];
+  disabled: boolean;
+  promptValue: string;
+  setPromptValue: React.Dispatch<React.SetStateAction<string>>;
+  setMessages: React.Dispatch<
+    React.SetStateAction<{ role: string; content: string }[]>
+  >;
+  handleChat: () => void;
+  topic: string;
+  handleInitialChat: () => Promise<void>;
 }) {
-	const messagesEndRef = useRef<HTMLDivElement>(null);
-	const scrollableContainerRef = useRef<HTMLDivElement>(null);
-	const [didScrollToBottom, setDidScrollToBottom] = useState(true);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+  const scrollableContainerRef = useRef<HTMLDivElement>(null);
+  const [didScrollToBottom, setDidScrollToBottom] = useState(true);
 
-	function scrollToBottom() {
-		messagesEndRef.current?.scrollIntoView({ behavior: "instant" });
-	}
+  function scrollToBottom() {
+    messagesEndRef.current?.scrollIntoView({ behavior: "instant" });
+  }
 
-	useEffect(() => {
-		if (didScrollToBottom) {
-			scrollToBottom();
-		}
-	}, [didScrollToBottom, messages]);
+  useEffect(() => {
+    if (didScrollToBottom) {
+      scrollToBottom();
+    }
+  }, [didScrollToBottom, messages]);
 
-	useEffect(() => {
-		let el = scrollableContainerRef.current;
-		if (!el) {
-			return;
-		}
+  useEffect(() => {
+    let el = scrollableContainerRef.current;
+    if (!el) {
+      return;
+    }
 
-		function handleScroll() {
-			if (scrollableContainerRef.current) {
-				const { scrollTop, scrollHeight, clientHeight } =
-					scrollableContainerRef.current;
-				setDidScrollToBottom(scrollTop + clientHeight >= scrollHeight);
-			}
-		}
+    function handleScroll() {
+      if (scrollableContainerRef.current) {
+        const { scrollTop, scrollHeight, clientHeight } =
+          scrollableContainerRef.current;
+        setDidScrollToBottom(scrollTop + clientHeight >= scrollHeight);
+      }
+    }
 
-		el.addEventListener("scroll", handleScroll);
+    el.addEventListener("scroll", handleScroll);
 
-		return () => {
-			el.removeEventListener("scroll", handleScroll);
-		};
-	}, []);
+    return () => {
+      el.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-	return (
-		<div className="flex grow flex-col gap-4 overflow-hidden">
-			<div className="flex grow flex-col overflow-hidden lg:px-4">
-				<p className="uppercase text-gray-900">
-					<b>Topic: </b>
-					{topic}
-				</p>
-				<div
-					ref={scrollableContainerRef}
-					className="mt-2 overflow-y-scroll rounded-lg border border-solid border-[#C2C2C2] bg-white px-5 lg:p-7"
-				>
-					{messages.length > 2 ? (
-						<div className="prose-sm max-w-5xl lg:prose lg:max-w-full">
-							{messages.slice(2).map((message, index) =>
-								message.role === "assistant" ? (
-									<div className="relative w-full" key={index}>
-										<Image
-											src={simpleLogo}
-											alt=""
-											className="absolute left-0 top-0 !my-0 size-7"
-										/>
-										<ReactMarkdown className="w-full pl-10">
-											{message.content}
-										</ReactMarkdown>
-									</div>
-								) : (
-									<p
-										key={index}
-										className="ml-auto w-fit rounded-xl bg-blue-500 p-4 font-medium text-white"
-									>
-										{message.content}
-									</p>
-								),
-							)}
-							<div ref={messagesEndRef} />
-						</div>
-					) : (
-						<div className="flex w-full flex-col gap-4 py-5">
-							{Array.from(Array(10).keys()).map((i) => (
-								<div
-									key={i}
-									className={`${
-										i < 5 && "hidden sm:block"
-									} h-10 animate-pulse rounded-md bg-gray-300`}
-									style={{ animationDelay: `${i * 0.05}s` }}
-								/>
-							))}
-						</div>
-					)}
-				</div>
-			</div>
+  return (
+    <div className="flex grow flex-col gap-4 overflow-hidden">
+      <div className="flex grow flex-col overflow-hidden lg:px-4">
+        <p className="uppercase text-gray-900">
+          <b>Topic: </b>
+          {topic}
+        </p>
+        <div
+          ref={scrollableContainerRef}
+          className="mt-2 overflow-y-scroll rounded-lg border border-solid border-[#C2C2C2] bg-white px-5 lg:p-7"
+        >
+          {messages.length > 2 ? (
+            <div className="prose-sm max-w-5xl lg:prose lg:max-w-full">
+              {messages.slice(2).map((message, index) =>
+                message.role === "assistant" ? (
+                  <div className="relative w-full" key={index}>
+                    <Image
+                      src={simpleLogo}
+                      alt=""
+                      className="absolute left-0 top-0 !my-0 size-7"
+                    />
+                    <ReactMarkdown className="w-full pl-10">
+                      {message.content}
+                    </ReactMarkdown>
+                  </div>
+                ) : (
+                  <p
+                    key={index}
+                    className="ml-auto w-fit rounded-xl bg-blue-500 p-4 font-medium text-white"
+                  >
+                    {message.content}
+                  </p>
+                ),
+              )}
+              <div ref={messagesEndRef} />
+            </div>
+          ) : (
+            <div className="flex w-full flex-col gap-4 py-5">
+              {Array.from(Array(10).keys()).map((i) => (
+                <div
+                  key={i}
+                  className={`${
+                    i < 5 && "hidden sm:block"
+                  } h-10 animate-pulse rounded-md bg-gray-300`}
+                  style={{ animationDelay: `${i * 0.05}s` }}
+                />
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
 
-			<RelatedTopics
-				topic={topic}
-				setPromptValue={setPromptValue}
-				handleInitialChat={handleInitialChat}
-			/>
+      <RelatedTopics
+        topic={topic}
+        setPromptValue={setPromptValue}
+        handleInitialChat={handleInitialChat}
+      />
 
-			<div className="bg-white lg:px-4">
-				<FinalInputArea
-					disabled={disabled}
-					promptValue={promptValue}
-					setPromptValue={setPromptValue}
-					handleChat={handleChat}
-					messages={messages}
-					setMessages={setMessages}
-				/>
-			</div>
-		</div>
-	);
+      <div className="bg-white lg:px-4">
+        <FinalInputArea
+          disabled={disabled}
+          promptValue={promptValue}
+          setPromptValue={setPromptValue}
+          handleChat={handleChat}
+          messages={messages}
+          setMessages={setMessages}
+        />
+      </div>
+    </div>
+  );
 }
