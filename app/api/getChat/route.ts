1d74ffbf96eb718f82fa641cb0a9f245e81a9dd8
Hassan El Mghari <hassan4709@gmail.com>
2025-06-06 08:56:02 -0700

    add rate limiting

--- app/api/getChat/route.ts
+++ app/api/getChat/route.ts
@@ -2,13 +2,38 @@ import {
   TogetherAIStream,
   TogetherAIStreamPayload,
 } from "@/utils/TogetherAIStream";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+import { headers } from "next/headers";
+
+let ratelimit: Ratelimit | undefined;
+
+// Add rate limiting if Upstash API keys are set, otherwise skip
+if (process.env.UPSTASH_REDIS_REST_URL) {
+  ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    // Allow 10 requests per day
+    limiter: Ratelimit.fixedWindow(10, "1440 m"),
+    analytics: true,
+    prefix: "llamatutor",
+  });
+}
 
 export async function POST(request: Request) {
   let { messages } = await request.json();
 
-  console.log("messages", messages);
+  if (ratelimit) {
+    const identifier = getIPAddress();
+
+    const { success } = await ratelimit.limit(identifier);
+    if (!success) {
+      return Response.json("No requests left. Try again in 24h.", {
+        status: 429,
+      });
+    }
+  }
+
   try {
-    console.log("[getChat] Fetching answer stream from Together API");
     const payload: TogetherAIStreamPayload = {
       model: "meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo",
       messages,
@@ -27,3 +52,14 @@ export async function POST(request: Request) {
 }
 
 export const runtime = "edge";
+
+function getIPAddress() {
+  const FALLBACK_IP_ADDRESS = "0.0.0.0";
+  const forwardedFor = headers().get("x-forwarded-for");
+
+  if (forwardedFor) {
+    return forwardedFor.split(",")[0] ?? FALLBACK_IP_ADDRESS;
+  }
+
+  return headers().get("x-real-ip") ?? FALLBACK_IP_ADDRESS;
+}
