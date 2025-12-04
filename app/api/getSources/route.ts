185784c1738d0d30cf748f4524df21b5f98cf94e
Riccardo Giorato <riccardoemanuelegiorato@gmail.com>
2025-12-04 17:50:47 +0100

    Update route.ts

--- app/api/getSources/route.ts
+++ app/api/getSources/route.ts
@@ -1,84 +1,29 @@
 import { NextResponse } from "next/server";
-import { z } from "zod";
+import { Exa } from "exa-js";
 
 let excludedSites = ["youtube.com"];
-let searchEngine: "bing" | "serper" = "serper";
 
 export async function POST(request: Request) {
   let { question } = await request.json();
 
   const finalQuestion = `what is ${question}`;
 
-  if (searchEngine === "bing") {
-    const BING_API_KEY = process.env["BING_API_KEY"];
-    if (!BING_API_KEY) {
-      throw new Error("BING_API_KEY is required");
-    }
-
-    const params = new URLSearchParams({
-      q: `${finalQuestion} ${excludedSites.map((site) => `-site:${site}`).join(" ")}`,
-      mkt: "en-US",
-      count: "6",
-      safeSearch: "Strict",
-    });
-
-    const response = await fetch(
-      `https://api.bing.microsoft.com/v7.0/search?${params}`,
-      {
-        method: "GET",
-        headers: {
-          "Ocp-Apim-Subscription-Key": BING_API_KEY,
-        },
-      },
-    );
-
-    const BingJSONSchema = z.object({
-      webPages: z.object({
-        value: z.array(z.object({ name: z.string(), url: z.string() })),
-      }),
-    });
-
-    const rawJSON = await response.json();
-    const data = BingJSONSchema.parse(rawJSON);
-
-    let results = data.webPages.value.map((result) => ({
-      name: result.name,
-      url: result.url,
-    }));
-
-    return NextResponse.json(results);
-    // TODO: Figure out a way to remove certain results like YT
-  } else if (searchEngine === "serper") {
-    const SERPER_API_KEY = process.env["SERPER_API_KEY"];
-    if (!SERPER_API_KEY) {
-      throw new Error("SERPER_API_KEY is required");
-    }
-
-    const response = await fetch("https://google.serper.dev/search", {
-      method: "POST",
-      headers: {
-        "X-API-KEY": SERPER_API_KEY,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        q: finalQuestion,
-        num: 9,
-      }),
-    });
-
-    const rawJSON = await response.json();
-
-    const SerperJSONSchema = z.object({
-      organic: z.array(z.object({ title: z.string(), link: z.string() })),
-    });
-
-    const data = SerperJSONSchema.parse(rawJSON);
-
-    let results = data.organic.map((result) => ({
-      name: result.title,
-      url: result.link,
-    }));
-
-    return NextResponse.json(results);
+  const EXA_API_KEY = process.env["EXA_API_KEY"];
+  if (!EXA_API_KEY) {
+    throw new Error("EXA_API_KEY is required");
   }
+
+  const exa = new Exa(EXA_API_KEY);
+
+  const results = await exa.search(finalQuestion, {
+    numResults: 9,
+    excludeDomains: excludedSites,
+  });
+
+  let mappedResults = results.results.map((result) => ({
+    name: result.title,
+    url: result.url,
+  }));
+
+  return NextResponse.json(mappedResults);
 }
